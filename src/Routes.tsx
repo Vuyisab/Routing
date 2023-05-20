a738c91a8dfd2743f4462f67ff5ed88b86bd18c4
Vuyisa Baza <vuyisa.baza@entelect.co.za>
2023-05-20 10:41:14 +0200

    Finished creating the app

---
+++ src/Routes.tsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ProductsPage } from "./pages/ProductsPage";
+import { ProductPage } from "./pages/ProductPage";
+import { ErrorPage } from "./pages/ErrorPage";
+import { HomePage } from "./pages/HomePage";
+import App from "./App";
+import { lazy, Suspense } from "react";
+
+//lazy imports
+const AdminPage = lazy(() => import("./pages/AdminPage"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "products/:id",
+        element: <ProductPage />,
+      },
+      {
+        path: "admin",
+        element: (
+          <Suspense
+            fallback={
+              <div className="text-center p-5 text-xl text-slate-900">
+                Loading...
+              </div>
+            }
+          >
+            <AdminPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+export function Routes() {
+  return <RouterProvider router={router} />;
+}
